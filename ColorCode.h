4ab130cdd4fa46f99fca6c1de85f2d5e18d02060
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-15 21:28:53 +0530

    Update and rename ColorPairTypes.h to ColorCode.h

---
+++ ColorCode.h
@@ -0,0 +1,41 @@
+#include <string>
+
+namespace TelCoColorCoder
+{
+   class ColorCode
+   {
+      public:
+         typedef enum
+         {
+            ColorCodePair__UNKNOWN = 0,
+            ColorCodePair__WHITE_BLUE,
+            ColorCodePair__WHITE_ORANGE,
+            ColorCodePair__WHITE_GREEN,
+            ColorCodePair__WHITE_BROWN,
+            ColorCodePair__WHITE_SLATE,
+            ColorCodePair__RED_BLUE,
+            ColorCodePair__RED_ORANGE,
+            ColorCodePair__RED_GREEN,
+            ColorCodePair__RED_BROWN,
+            ColorCodePair__RED_SLATE,
+            ColorCodePair__BLACK_BLUE,
+            ColorCodePair__BLACK_ORANGE,
+            ColorCodePair__BLACK_GREEN,
+            ColorCodePair__BLACK_BROWN,
+            ColorCodePair__BLACK_SLATE,
+            ColorCodePair__YELLOW_BLUE,
+            ColorCodePair__YELLOW_ORANGE,
+            ColorCodePair__YELLOW_GREEN,
+            ColorCodePair__YELLOW_BROWN,
+            ColorCodePair__YELLOW_SLATE,
+            ColorCodePair__VIOLET_BLUE,
+            ColorCodePair__VIOLET_ORANGE,
+            ColorCodePair__VIOLET_GREEN,
+            ColorCodePair__VIOLET_BROWN,
+            ColorCodePair__VIOLET_SLATE
+         } ColorCodePair;
+ 
+         ColorCode() {}
+         std::string MapColorCodePairToPairNumber(const ColorCodePair &colorCodePair);
+   };
+}
