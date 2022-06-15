122f881a26c797c7a5c721bb2d8e1b9dfc0c5a82
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-15 22:33:14 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,14 +1,11 @@
 #include "ColorPair.h"
 
-#define MAJORCOLORELEMENT 5
-#define MINORCOLORELEMENT 5
-
 namespace TelCoColorCoder
 {
-    const char* MajorColorNames[MAJORCOLORELEMENT] = { "White", "Red", "Black", "Yellow", "Violet" };
+    const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
     int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-    const char* MinorColorNames[MINORCOLORELEMENT] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+    const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
     int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
     ColorPair::ColorPair(MajorColor major, MinorColor minor): majorColor(major), minorColor(minor)
