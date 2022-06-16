521a1a530c35eac88e3c6f415df4c1e51ae75ab4
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-16 10:28:39 +0530

    Update ColorCode.h

--- ColorCode.h
+++ ColorCode.h
@@ -36,6 +36,6 @@ namespace TelCoColorCoder
          } ColorCodePair;
  
          ColorCode() {}
-         inline std::string MapColorCodePairToPairNumber(ColorCodePair colorCodePair);
+         std::string MapColorCodePairToPairNumber(ColorCodePair colorCodePair);
    };
 }
