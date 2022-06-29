2d9b6dd48b7add9f76b024c1922b99e8d886baef
MadhuMohanPR <105207808+MadhuMohanPR@users.noreply.github.com>
2022-06-29 16:13:10 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include "TestColorCoder.h"
-#include "ColorCode.h"
 
 int main()
 {
-    TelCoColorCoder::ColorPair colorPair;
-    TelCoColorCoder::ColorCode colorCode;
-    std::cout << "Color Code Pair : " << colorCode.MapColorCodePairToPairNumber(colorCode.ColorCodePair__BLACK_SLATE) << std::endl;
-    std::cout << "Color Code Pair : " << colorCode.MapColorCodePairToPairNumber(colorCode.ColorCodePair__WHITE_BROWN) << std::endl;
-
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
